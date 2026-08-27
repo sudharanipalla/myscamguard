6fe255066bcc8e1a6706f82f9c3a9abd2b9febee
sudharanipalla <sudharani.palla@gmail.com>
2026-08-27 20:27:56 +0530

    Update pydanticcode.py

--- pydanticcode.py
+++ pydanticcode.py
@@ -21,7 +21,7 @@ JSON:
 """
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash",
-                             google_api_key="AQ.Ab8RN6L1V3l5Ki6f8xy3SsF3si2IXt0mZBJHu6pnA5kn4hLIMA")
+                             google_api_key="")
 
 response = llm.invoke(prompt.format(resume=resume_text, format_instructions=format_instructions))
 
